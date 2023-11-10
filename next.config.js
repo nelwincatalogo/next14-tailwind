3f0333e2c8a39ee78076e09cb913ec0244a382ba
Nelwin Catalogo <catalogonelwin@gmail.com>
2023-11-10 11:33:02 +0800

    💡 add comments in nextjs config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // To enable a static export
   output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
   swcMinify: true,
+  // prevent double rerendering
   reactStrictMode: false,
 };
 
