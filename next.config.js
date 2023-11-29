a939a0655544271a328a1734e4332b20e2fb83ed
Nelwin Catalogo <catalogonelwin@gmail.com>
2023-11-29 20:32:11 +0800

    🔧 update config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   output: 'export',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  skipTrailingSlashRedirect: true,
   swcMinify: true,
   // prevent double rerendering
   reactStrictMode: false,
