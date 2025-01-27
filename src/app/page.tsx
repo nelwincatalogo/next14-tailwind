a942e2e51ef7ec13eab5cad37e2a8d1355568f2d
Nelwin Catalogo <catalogonelwin@gmail.com>
2025-01-27 20:53:03 +0800

    💄 seperate the template sample for fast removal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,11 @@
 'use client';
 
-import { SvgSpinnersBarsRotateFade } from '@/components/icons/SvgSpinnersBarsRotateFade';
-import { ButtonLoading } from '@/components/loading/ButtonLoading';
-import LoadingDots2 from '@/components/loading/loading-dots-2';
-import LoadingDots from '@/components/loading/LoadingDots';
-import { AlertDialogDemo } from '@/components/sample/AlertDialogSample';
-import { DialogCloseButton } from '@/components/sample/DialogSample';
-import ReactAlertSample from '@/components/sample/ReactAlertSample';
-import { DrawerDialogDemo } from '@/components/sample/responsive-dialog';
-import { AdvancedDateRangePicker } from '@/components/ui-collection/advance-range-date-picker';
-import { Badge } from '@/components/ui/badge';
+import Template from '@/components/sample/template';
 
 export default function Home() {
   return (
-    <main className="bg-gray-200 font-sans">
-      <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-        <h1 className="text-center text-4xl font-bold text-red-500">Next-Tailwind Starter Template</h1>
-
-        <div className="mt-4 rounded-full bg-gray-300 px-3 py-1 text-xs">Available Fonts</div>
-        <div className="flex gap-4">
-          <h1 className="text-center font-sans text-4xl font-bold text-green-500">Geist Font</h1>
-          <h1 className="text-center font-inter text-4xl font-bold text-amber-500">Inter Font</h1>
-          <h1 className="text-center font-poppins text-4xl font-bold text-blue-500">Popppins Font</h1>
-        </div>
-
-        <div className="mt-4 rounded-full bg-gray-300 px-3 py-1 text-xs">Toast Example</div>
-        <ReactAlertSample />
-
-        <div className="mt-4 rounded-full bg-gray-300 px-3 py-1 text-xs">Dialog Example</div>
-        <div className="flex gap-4">
-          <AlertDialogDemo />
-          <DialogCloseButton />
-          <DrawerDialogDemo />
-        </div>
-
-        <div className="mt-4 rounded-full bg-gray-300 px-3 py-1 text-xs">Badge Example</div>
-        <div className="flex gap-4">
-          <Badge variant="success">Success</Badge>
-          <Badge variant="warning">Warning</Badge>
-          <Badge variant="info">Info</Badge>
-          <Badge variant="error">Error</Badge>
-          <Badge variant="gray">Gray</Badge>
-          <Badge variant="orange">Orange</Badge>
-        </div>
-
-        <div className="mt-4 rounded-full bg-gray-300 px-3 py-1 text-xs">Advance Date Picker</div>
-        <div className="flex gap-4">
-          <AdvancedDateRangePicker />
-        </div>
-
-        <div className="mt-4 rounded-full bg-gray-300 px-3 py-1 text-xs">Loading Example</div>
-        <div className="flex items-center gap-4">
-          <SvgSpinnersBarsRotateFade className="size-4 animate-spin" />
-          <ButtonLoading />
-          <LoadingDots />
-          <LoadingDots2 />
-          <LoadingDots2 message="Please wait" dots="." />
-        </div>
-      </div>
+    <main className="font-sans">
+      <Template />
     </main>
   );
 }
